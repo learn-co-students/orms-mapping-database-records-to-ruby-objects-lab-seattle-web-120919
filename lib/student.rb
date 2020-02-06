091b97781a9c03b8bd79b1bd0986b2f153206b60
kkirby16 <kkirby16@my.whitworth.edu>
2020-02-05 19:53:23 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,124 +1,44 @@
-require 'pry'
-
 class Student
   attr_accessor :id, :name, :grade
 
-    def self.new_from_db(row)
-      new_student = self.new  # self.new is the same as running Song.new
-      new_student.id = row[0]
-      new_student.name =  row[1]
-      new_student.grade = row[2]
-      new_student  # return the newly created instance
-    end
-    # create a new Student object given a row from the database
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
 
-
-  def self.all
+  def save
     sql = <<-SQL
-    SELECT *
-    FROM students
-    SQL
-#why do we end again with this SQL here?
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end 
-end
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+  SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * 
-    FROM students 
-    WHERE name = ? 
-    LIMIT 1
-  SQL
-
-    DB[:conn].execute(sql, name).map do |row| 
-      self.new_from_db(row)
-    end.first
-  end 
-
-  def self.all_students_in_grade_9
-    sql = <<-SQL
     SELECT *
-    FROM students 
-    WHERE grade = 9
-  SQL
-
-  DB[:conn].execute(sql).map do |row| #why do we just do execute sql here rather than execute sql and grade?
-      self.new_from_db(row)
-    end
-  end 
-
-  def self.students_below_12th_grade
-    sql = <<-SQL
-    SELECT *
-    FROM students 
-    WHERE grade <= 11
-  SQL
-
-  DB[:conn].execute(sql).map do |row| #why do we just do execute sql here rather than execute sql and grade?
-      self.new_from_db(row)
-    end
-  end 
-
-  def self.first_X_students_in_grade_10(number)
-    sql = <<-SQL 
-    SELECT * 
-    FROM students 
-    WHERE grade = 10 LIMIT ? 
-    SQL
-    
-    DB[:conn].execute(sql, number).map do |row| #<---- for the part we put in the parentheses after execute do we put number after since it is an argument of the method?
-      self.new_from_db(row) 
-    end 
-  end 
-
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-      SELECT *
       FROM students
-      WHERE grade = 10
-    SQL
- 
-    DB[:conn].execute(sql).map do |row|
+      WHERE name = ?
+      LIMIT 1
+      SQL
+
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
 
-  def self.all_students_in_grade_X(grade)
-    sql =<<-SQL
-    SELECT *
-    FROM students
-    WHERE grade = ?
-    SQL
-
-    DB[:conn].execute(sql, grade).map do |row| 
-      self.new_from_db(row)
-  end 
-end 
-
-  def save
-    if self.id
-       self.update
-    else 
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
-  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
+      id INTEGER PRIMARY KEY, 
       name TEXT,
       grade TEXT
-    )
-    SQL
+      )
+      SQL
 
     DB[:conn].execute(sql)
   end
@@ -126,5 +46,77 @@ end
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+      FROM students
+      WHERE grade = 9
+      SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+      FROM students
+      WHERE grade <= 11
+      SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.all
+    sql = <<-SQL
+    SELECT *
+      FROM students
+      SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+      SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+      FROM students
+      WHERE grade = 10
+      SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT *
+      FROM students
+      WHERE grade = ?
+      SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
